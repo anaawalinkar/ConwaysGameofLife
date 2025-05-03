d3b54896377f9840aa5e0d04ab5d86da6930669a
anaawalinkar <awalinkar1@gmail.com>
2025-05-03 16:17:35 -0400

    dbpassupdate

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $DB_HOST = "localhost";
 $DB_USER = "aawalinkar1";
-$DB_PASS = "";   
+$DB_PASS = "aawalinkar1";   
 $DB_NAME = "aawalinkar1";
 
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
