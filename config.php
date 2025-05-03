50fc8e1ef7421c741ffc76431671ebdb1e650f0a
anaawalinkar <awalinkar1@gmail.com>
2025-05-03 16:12:26 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$DB_HOST = //put your db info
-$DB_USER = //put your db info
-$DB_PASS = //put your db info    
-$DB_NAME = //put your db info
+$DB_HOST = "localhost";
+$DB_USER = "aawalinkar1";
+$DB_PASS = "";   
+$DB_NAME = "game_of_life";
 
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
