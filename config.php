21045907c7b598eaa28263e3479a73cea26ad707
anaawalinkar <awalinkar1@gmail.com>
2025-05-03 16:15:16 -0400

    dbname change

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $DB_HOST = "localhost";
 $DB_USER = "aawalinkar1";
 $DB_PASS = "";   
-$DB_NAME = "game_of_life";
+$DB_NAME = "aawalinkar1";
 
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
